003498909b6c4baad61d5f90f9077a81ccb1a29c
tomasantolaria <109923421+tomasantolaria@users.noreply.github.com>
2023-03-22 10:13:37 -0300

    Add files via upload

---
+++ TiposDeDatos.java
@@ -0,0 +1,19 @@
+package Sesion1.Ejercicio;
+
+public class TiposDeDatos {
+    public static void main(String[] args) {
+
+        System.out.println("byte");
+        System.out.println("short");
+        System.out.println("int");
+        System.out.println("long");
+        System.out.println("     ");
+        System.out.println("float");
+        System.out.println("double");
+        System.out.println("       ");
+        System.out.println("boolean");
+        System.out.println("        ");
+        System.out.println("char");
+        System.out.println("String");
+    }
+}
